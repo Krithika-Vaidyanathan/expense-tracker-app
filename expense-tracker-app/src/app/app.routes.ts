64d787628299ce1fb93995832824278d957b3ecd
krithika <krithikavaidyanathan13@gmail.com>
2025-10-22 16:09:30 +0530

    initial-version-project

---
+++ expense-tracker-app/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { BudgetDetailsComponent } from './pages/budget-details/budget-details.component';
+import { CreateAccountComponent } from './pages/create-account/create-account.component';
+
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'home' },
+    { path: 'home', component: HomeComponent },
+    { path: 'details/:id', component: BudgetDetailsComponent },
+    { path: 'create-account', component: CreateAccountComponent }
+];
